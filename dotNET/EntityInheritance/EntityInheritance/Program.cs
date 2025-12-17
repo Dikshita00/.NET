48c49b04e2f823c59a3563f3a63b68421474a37d
Dikshita Singh <159521918+Dikshita00@users.noreply.github.com>
2025-12-17 14:56:18 +0530

    Add files via upload

---
+++ dotNET/EntityInheritance/EntityInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using EntityInheritance.logic;
+
+namespace EntityInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            //UI
+            #region Demo 01
+            Person person = new Person();
+            person.first_name = "John";
+            person.last_name = "Doe";
+            Console.WriteLine(person);
+            Console.WriteLine("Name : {0} {1}", person.first_name, person.last_name);
+            #endregion
+
+            #region Demo 02
+            Employee employee = new Employee();
+            employee.employee_id = 1001;
+            Console.WriteLine("ID: {2}, Name : {0} {1} {2}", person.first_name, person.last_name, employee.employee_id);
+            #endregion
+
+        }
+    }
+
+}
---
+++ dotNET/EntityInheritance/EntityInheritance/logic/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityInheritance.logic
+{
+
+    /*
+     In C#, internal is an access modifier.
+
+    It specifies that the type or member is accessible only within the same assembly (project).
+
+    An assembly is basically the compiled output of your project (like a .dll or .exe).
+     */
+    internal class Person
+    {
+        public string first_name;
+        public string last_name;
+
+    }
+    internal class Employee : Person {
+
+        public int employee_id;
+    
+    }
+}
